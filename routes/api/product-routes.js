055594857f02ceb60d865f1e95ba8f78fea54542
[David Cheptegei] <[davidchepte@gmail.com]>
2023-02-18 14:27:32 -0500

    BUILD & FIX: Added eslint to style my syntax accordingly and fixed all syntax issues

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,6 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+/* eslint-disable camelcase */
+const router = require('express').Router()
+const { Product, Category, Tag, ProductTag } = require('../../models')
 
 // The `/api/products` endpoint
 
@@ -8,50 +9,50 @@ router.get('/', async (req, res) => {
   // find all products and includes its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
       include: [{
         model: Category,
-				attributes: ["id", "category_name"],
+        attributes: ['id', 'category_name']
       },
       {
-				model: Tag,
-				attributes: ["id", "tag_name"],
-				through: "ProductTag",
-			}]
-    });
-    res.status(200).json(productData);
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        through: 'ProductTag'
+      }]
+    })
+    res.status(200).json(productData)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id` and includes its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
       include: [{
         model: Category,
-				attributes: ["id", "category_name"],
+        attributes: ['id', 'category_name']
       },
       {
-				model: Tag,
-				attributes: ["id", "tag_name"],
-				through: "ProductTag",
-			}]
-    });
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        through: 'ProductTag'
+      }]
+    })
 
     if (!productData) {
-      res.status(404).json({ message: 'No location found with this id!' });
-      return;
+      res.status(404).json({ message: 'No location found with this id!' })
+      return
     }
 
-    res.status(200).json(productData);
+    res.status(200).json(productData)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
 // create new product
 router.post('/', (req, res) => {
@@ -67,65 +68,67 @@ router.post('/', (req, res) => {
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
+        // eslint-disable-next-line camelcase
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
+            // eslint-disable-next-line camelcase
+            tag_id
+          }
+        })
+        return ProductTag.bulkCreate(productTagIdArr)
       }
       // if no product tags, just respond
-      res.status(200).json(product);
+      res.status(200).json(product)
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-});
+      console.log(err)
+      res.status(400).json(err)
+    })
+})
 
 // update product
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
-      id: req.params.id,
-    },
+      id: req.params.id
+    }
   })
     .then((product) => {
       // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
+      return ProductTag.findAll({ where: { product_id: req.params.id } })
     })
     .then((productTags) => {
       // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
+      const productTagIds = productTags.map(({ tag_id }) => tag_id)
       // create filtered list of new tag_ids
       const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
           return {
             product_id: req.params.id,
-            tag_id,
-          };
-        });
+            tag_id
+          }
+        })
       // figure out which ones to remove
       const productTagsToRemove = productTags
         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
+        .map(({ id }) => id)
 
       // run both actions
       return Promise.all([
         ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
+        ProductTag.bulkCreate(newProductTags)
+      ])
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
       // console.log(err);
-      res.status(400).json(err);
-    });
-});
+      res.status(400).json(err)
+    })
+})
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
@@ -134,17 +137,17 @@ router.delete('/:id', async (req, res) => {
       where: {
         id: req.params.id
       }
-    });
+    })
 
     if (!productData) {
-      res.status(404).json({ message: 'No location found with this id!' });
-      return;
+      res.status(404).json({ message: 'No location found with this id!' })
+      return
     }
 
-    res.status(200).json(productData);
+    res.status(200).json(productData)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
