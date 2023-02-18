865ec2f7df062613b515f746abd991b08fc904ad
[David Cheptegei] <[davidchepte@gmail.com]>
2023-02-18 06:19:48 -0500

    BUILD: added the product and tag router codes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { findByPk } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
